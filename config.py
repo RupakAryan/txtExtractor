bbbb9ce74038dc3d64d6bd44db70e5d9a674beb4
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-11-04 21:50:04 +0530

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8456934225:AAG1pSPFq0YTGAQp_iACnaO8mu_0zI9J3qY")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8400419495:AAHPVhXaN4uKeiCGjvNGhic5S1SZWdH2YFc")
     API_ID = int(os.environ.get("API_ID", "29978901"))
     API_HASH = os.environ.get("API_HASH", "500fc876c5356cf04ed3698912dc2edf")
     AUTH_USERS = os.environ.get("AUTH_USERS", "5776977809")
